f1ae2eacf8a412e214c5e32e3fd64e24b79e3e2f
AlexisBlossom <ajtauer00@gmail.com>
2025-06-14 21:31:58 -0400

    player setup, stats, and resistances defined

---
+++ player.py
@@ -0,0 +1,85 @@
+def Player():
+    pass
+
+
+    def move_speed():
+        pass
+
+    def armor():
+        pass
+
+    def intelligence():
+        pass
+
+    def strength():
+        pass
+
+    def vitality():
+        pass
+
+    def agility():
+        pass
+
+    def charisma():
+        pass
+
+    def dexterity():
+        pass
+
+    def health():
+        pass
+
+    def power():
+        pass
+
+    def magic_power():
+        pass
+
+    def action_speed():
+        pass
+
+    def magic_action_speed():
+        pass
+
+    def regeneration():
+        pass
+
+    def level():
+        pass
+
+    def armor_pen():
+        pass
+
+    def magic_armor_pen():
+        pass
+
+    def magic_healing():
+        pass
+
+    def phys_healing():
+        pass
+
+    def life_leech():
+        pass
+
+    
+def Resistance():
+    pass
+
+    def light_magic_res():
+        pass
+
+    def dark_magic_res():
+        pass
+
+    def fire_magic_res():
+        pass
+
+    def water_magic_res():
+        pass
+
+    def earth_magic_res():
+        pass
+
+    def air_magic_res():
+        pass
