05b65f57a5e9aa1cc8adf67ec26ee7eda5d371b0
AlexisBlossom <ajtauer00@gmail.com>
2025-06-15 07:28:21 -0400

    put classes into a folder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Damage_Types import *
 from player import *
+import Classes.Barbarian,Classes.Bard,Classes.Ranger,Classes.Rogue,Classes.Cleric,Classes.Druid,Classes.Fighter,Classes.Paladin,Classes.Priest,Classes.Sorcerer,Classes.Spellsword,Classes.Spellwrite,Classes.Warlock,Classes.Wizard
 def main():
     pass
 
